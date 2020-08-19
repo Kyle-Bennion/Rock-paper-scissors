05aeef524a8acb15d9f8aefc18f3a4e6e676a2e7
Kyle Bennion <bilekennion@hotmail.com>
2020-08-19 15:10:57 -0600

    basic function and set up done

---
+++ app.js
@@ -0,0 +1,31 @@
+
+function play(playerChoice) {
+  let compChoice = 'rock'
+
+  if (compChoice == playerChoice) {
+    console.log('tie')
+  }
+  else if (playerChoice == 'scissors') {
+    if (compChoice == 'rock') {
+      console.log('Lose')
+    }
+    else {
+      console.log('win')
+    }
+  }
+  else if (playerChoice == 'paper') {
+    if (compChoice == 'rock') {
+      console.log('win')
+    }
+    else {
+      console.log('Lose')
+    }
+  }
+  else if (playerChoice == 'rock') {
+    if (compChoice == '')
+  }
+
+}
+
+  }
+}
